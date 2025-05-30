f9c6bd58884119b995733061d523c7d5334b3530
FELIPE EDUARDO DE CAMARGO <felipe.ecamargo@senacsp.edu.br>
2025-05-29 21:48:09 -0300

    senac

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# barbearia-backend
+# barbearia-backend aprentaçao
+
+API REST com Node.js e MongoDB
+
+.A API será baseada no framework Express para tratar as requisições e enviar as respostas aos endpoints;
+.Vamos utilizar o MongoDB como banco, porém com a ODM Mongoose;
+.Realizaremos as operações no MongoDB Atlas;
+.O pacote Nodemon vai ajudar a atualizar o código em tempo real da aplicação;
+.A API será testada pelo software Postman.
+
+# barbearia-backend aprentaçao/o que vamos precisar
+
+Node.js instalado na máquina, para rodar o projeto e poder utilizar o npm para gerenciar os pacotes da aplicação;
+Editor de código (sugestão: VS Code);
+Postman, para teste de API;
+Uma conta no MongoDB Atlas ou MongoDB instalado na máquina.
+
+
+# comados utilizado no terminal
+
+npm init -y
+nom install express nodmon mongoose
---
+++ index.js
@@ -0,0 +1,53 @@
+// config inicial
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+// forma de ler json / middlewares
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+)
+
+app.use(express.json())
+
+// rota inicial / endpoint
+app.get('/', (req , res) => {
+// mostrar requisisao
+
+res.json({massage: 'oi express!!'})
+
+})
+
+
+
+
+// entregar um porta
+const DB_USER = 'felipecamargobr10'
+const DB_PASSWORD = encodeURIComponent('qLKc6jUsEvFeveFA')
+mongoose
+.connect(
+    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apiuc12.6dazp8v.mongodb.net/?retryWrites=true&w=majority&appName=apiuc12`
+)
+.then(() => {
+    console.log("conectamos ao mongodb!")
+    app.listen(3000) 
+})
+.catch((err) => console.log(err))
+
+
+
+
+
+
+
+
+
+
+
+// entregar um porta felipecamargobr10 qLKc6jUsEvFeveFA
+
+//mongodb+srv://felipecamargobr10:qLKc6jUsEvFeveFA@apiuc12.6dazp8v.mongodb.net/?retryWrites=true&w=majority&appName=apiuc12
+
+// mongodb+srv://felipecamargobr10:qLKc6jUsEvFeveFA@apiuc12.6dazp8v.mongodb.net/?retryWrites=true&w=majority&appName=apiuc12
